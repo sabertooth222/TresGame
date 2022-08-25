87720551feb3b0529428a1dc2e700dcaefe0f928
Joseph Lee <17460257+josephl70@users.noreply.github.com>
2022-08-25 13:30:59 -0400

    Pushed Initial Factory Code

--- Source/ScaleformUI/ScaleformUIEd/Private/ScaleformUI_Factory.cpp
+++ Source/ScaleformUI/ScaleformUIEd/Private/ScaleformUI_Factory.cpp
@@ -8,80 +8,72 @@
 #include "Paths.h"
 #include "SQEX_KineDriverData.h"
 
-USQEX_KineDriverData_Factory::USQEX_KineDriverData_Factory()
+UScaleformUI_Factory::UScaleformUI_Factory()
 {
-	Formats.Add(TEXT("xml;KineDriver Data"));
-	bCreateNew = true;
-	bEditAfterNew = true;
+	Formats.Add(TEXT("swf;SWF Movie"));
+	Formats.Add(TEXT("gfx;SWF Movie (Stripped)"));
 	bEditorImport = true;
-	SupportedClass = USQEX_KineDriverData::StaticClass();
+	SupportedClass = USwfMovie::StaticClass();
 }
 
-static bool bSoundFactorySuppressImportOverwriteDialog = false;
-
-uint32 USQEX_KineDriverData_Factory::GetMenuCategories() const { return EAssetTypeCategories::Physics; }
-
-UObject * USQEX_KineDriverData_Factory::FactoryCreateNew(UClass * InClass, UObject * InParent, FName InName, EObjectFlags flags, UObject * Cntext, FFeedbackContext * Warn)
+UObject* UScaleformUI_Factory::FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled)
 {
-	return NewObject<USQEX_KineDriverData>(InParent, InClass, InName, flags);
-}
-
-UObject* USQEX_KineDriverData_Factory::FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled)
-{
-	FString PackagePath = FPackageName::GetLongPackagePath(InParent->GetOutermost()->GetName());
-
-	USQEX_KineDriverData* ImportedAsset = NewObject<USQEX_KineDriverData>(InParent, Name, Flags);
+	USwfMovie* ImportedAsset = NewObject<USwfMovie>(InParent, Name, Flags);
+	ImportedAsset->bSetSRGBOnImportedTextures = false;
+	ImportedAsset->TextureRescale = EFlashTextureRescale::FlashTextureScale_High;
+	ImportedAsset->bPackTextures = false;
+	ImportedAsset->PackTextureSize = 1024;
+	ImportedAsset->TextureFormat = "TGA";
 	FString FilePath = FPaths::GetPath(CurrentFilename);
 	FPaths::MakePathRelativeTo(FilePath, *FPaths::GameDir());
+	ImportedAsset->SourceFile = FilePath + "/" + FPaths::GetCleanFilename(CurrentFilename);
+	ImportedAsset->SourceFileTimestamp = IFileManager::Get().GetTimeStamp(*UFactory::CurrentFilename).ToString();
+	ImportedAsset->RawData.Empty(BufferEnd - Buffer);
+	ImportedAsset->RawData.AddUninitialized(BufferEnd - Buffer);
+	FMemory::Memcpy(ImportedAsset->RawData.GetData(), Buffer, ImportedAsset->RawData.Num());
 	return ImportedAsset;
 }
-
-bool USQEX_KineDriverData_Factory::CanReimport(UObject* Obj, TArray<FString>& OutFilenames)
+bool UScaleformUI_Factory::CanReimport(UObject* Obj, TArray<FString>& OutFilenames)
 {
-	USQEX_KineDriverData* ImportedAsset = Cast<USQEX_KineDriverData>(Obj);
+	USwfMovie* ImportedAsset = Cast<USwfMovie>(Obj);
 	if (ImportedAsset)
 	{
-		//OutFilenames.Add(FPaths::ConvertRelativePathToFull(FPaths::GameDir(), ImportedAsset->SourceFile));
+		OutFilenames.Add(FPaths::ConvertRelativePathToFull(FPaths::GameDir(), ImportedAsset->SourceFile));
 		return true;
 	}
 	return false;
 }
-
-void USQEX_KineDriverData_Factory::SetReimportPaths(UObject* Obj, const TArray<FString>& NewReimportPaths)
+void UScaleformUI_Factory::SetReimportPaths(UObject* Obj, const TArray<FString>& NewReimportPaths)
 {
-	USQEX_KineDriverData* ImportedAsset = Cast<USQEX_KineDriverData>(Obj);
+	USwfMovie* ImportedAsset = Cast<USwfMovie>(Obj);
 	if (ImportedAsset && ensure(NewReimportPaths.Num() == 1))
 	{
 		FString FilePath = FPaths::GetPath(NewReimportPaths[0]);
 		FPaths::MakePathRelativeTo(FilePath, *FPaths::GameDir());
-		//ImportedAsset->SourceFile = FilePath + "/" + FPaths::GetCleanFilename(NewReimportPaths[0]);
+		ImportedAsset->SourceFile = FilePath + "/" + FPaths::GetCleanFilename(NewReimportPaths[0]);
 	}
 }
-
-EReimportResult::Type USQEX_KineDriverData_Factory::Reimport(UObject* Obj)
+EReimportResult::Type UScaleformUI_Factory::Reimport(UObject* Obj)
 {
-	USQEX_KineDriverData* ImportedAsset = Cast<USQEX_KineDriverData>(Obj);
+	USwfMovie* ImportedAsset = Cast<USwfMovie>(Obj);
 	if (!ImportedAsset) { return EReimportResult::Failed; }
-	//const FString Filename = FPaths::ConvertRelativePathToFull(FPaths::GameDir(), ImportedAsset->SourceFile);
+	const FString Filename = FPaths::ConvertRelativePathToFull(FPaths::GameDir(), ImportedAsset->SourceFile);
 	bool OutCanceled = false;
-	/*if (FPaths::GetExtension(Filename).Equals(TEXT("xml"), ESearchCase::IgnoreCase))
+	if (FPaths::GetExtension(Filename).Equals(TEXT("gfx"), ESearchCase::IgnoreCase) || FPaths::GetExtension(Filename).Equals(TEXT("swf"), ESearchCase::IgnoreCase))
 	{
 		if (ImportObject(ImportedAsset->GetClass(), ImportedAsset->GetOuter(), *ImportedAsset->GetName(), RF_Public | RF_Standalone, Filename, nullptr, OutCanceled) != nullptr)
 		{
 			ImportedAsset->Modify();
 			ImportedAsset->MarkPackageDirty();
-			//ImportedAsset->SourceFileTimestamp = IFileManager::Get().GetTimeStamp(*UFactory::CurrentFilename).ToString();
+			ImportedAsset->SourceFileTimestamp = IFileManager::Get().GetTimeStamp(*UFactory::CurrentFilename).ToString();
 			return EReimportResult::Succeeded;
 		}
 		else
 		{
-			/*if (OutCanceled) { UE_LOG(LogTemp, Warning, TEXT("SWF Movie Import Canceled!")); }
+			if (OutCanceled) { UE_LOG(LogTemp, Warning, TEXT("SWF Movie Import Canceled!")); }
 			else { UE_LOG(LogTemp, Warning, TEXT("SWF Movie Import Failed!")); }
 			return EReimportResult::Failed;
 		}
-	}*/
-	//else { return EReimportResult::Failed; }
-
-
-	return EReimportResult::Succeeded;
+	}
+	else { return EReimportResult::Failed; }
 }
