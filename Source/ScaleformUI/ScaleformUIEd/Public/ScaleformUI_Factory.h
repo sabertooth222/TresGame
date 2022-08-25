146287cbf2f578fb9d6adbd16df1f9bab27442b0
Joseph Lee <17460257+josephl70@users.noreply.github.com>
2022-08-25 13:30:59 -0400

    Pushed Initial Factory Code

--- Source/ScaleformUI/ScaleformUIEd/Public/ScaleformUI_Factory.h
+++ Source/ScaleformUI/ScaleformUIEd/Public/ScaleformUI_Factory.h
@@ -10,15 +10,10 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(hidecategories = Object)
 class SCALEFORMUIED_API UScaleformUI_Factory : public UFactory, public FReimportHandler
 {
-	GENERATED_BODY()
-	
-	UScaleformUI_Factory();
-	virtual uint32 GetMenuCategories() const override;
-	FText GetDisplayName() const override { return FText::FromString(TEXT("Scaleform Gfx")); }
-	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Cntext, FFeedbackContext* Warn) override;
+	GENERATED_UCLASS_BODY()
 	virtual UObject* FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled) override;
 	virtual bool CanReimport(UObject* Obj, TArray<FString>& OutFilenames) override;
 	virtual void SetReimportPaths(UObject* Obj, const TArray<FString>& NewReimportPaths) override;
