d82a02ac2fb285dfb23121ee6040c1766186eb91
Narknon <narknon0@gmail.com>
2022-09-02 00:54:43 -0400

    delegatedelegate

--- Source/TresGame/Public/TresParamDispatcherPostInterpChangeDelegateDelegate.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "CoreMinimal.h"
-#include "TresParamDispatcherPostInterpChangeDelegateDelegate.generated.h"
-
-UDELEGATE(BlueprintCallable) DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTresParamDispatcherPostInterpChangeDelegate, FName, ParameterName);
-
--- Source/TresGame/Public/TresParamDispatcherUpdateDelegateDelegate.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "CoreMinimal.h"
-#include "TresParamDispatcherUpdateDelegateDelegate.generated.h"
-
-UDELEGATE(BlueprintCallable) DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTresParamDispatcherUpdateDelegate);
-
