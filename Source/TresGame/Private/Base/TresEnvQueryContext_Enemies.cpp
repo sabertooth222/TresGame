d5592cfdff6c67f21de87b211b3b827d74ddc5ca
Narknon <narknon0@gmail.com>
2022-09-05 21:56:53 -0400

    before big find/replace

--- Source/TresGame/Private/Base/TresEnvQueryContext_Enemies.cpp
+++ Source/TresGame/Private/Base/TresEnvQueryContext_Enemies.cpp
@@ -1,5 +1,4 @@
 #include "TresEnvQueryContext_Enemies.h"
 
-UTresEnvQueryContext_Enemies::UTresEnvQueryContext_Enemies() {
+UTresEnvQueryContext_Enemies::UTresEnvQueryContext_Enemies(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 }
-
