749b7d4e452babca6a2a61c6d60a99ffe31ea7d4
sabertooth222 <sabertooth222@gmail.com>
2026-06-03 02:07:44 -0400

    stuff

--- Source/TresGame/Private/TresNpcPawn_c_npc.cpp
+++ Source/TresGame/Private/TresNpcPawn_c_npc.cpp
@@ -1,6 +1,53 @@
 #include "TresNpcPawn_c_npc.h"
+//CROSS-MODULE INCLUDE V2: -ModuleName=Engine -ObjectName=EAutoPossessAI -FallbackName=EAutoPossessAI
+//CROSS-MODULE INCLUDE V2: -ModuleName=Engine -ObjectName=ESQEX_ObjectType -FallbackName=ESQEX_ObjectType
 
-class AActor;
+ATresNpcPawn_c_npc::ATresNpcPawn_c_npc(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+    this->bCanBeDamaged = false;
+    this->m_ObjType = ESQEX_ObjectType::SQEX_OBJ_TYPE_AVATAR;
+    this->m_bPermissionLockOn = false;
+    this->AutoPossessAI = EAutoPossessAI::Disabled;
+    this->MyLockOnSetBoneName = TEXT("atama");
+    const FProperty* p_MyAtkColl = GetClass()->FindPropertyByName("MyAtkColl");
+    (*p_MyAtkColl->ContainerPtrToValuePtr<UTresAtkCollComponent*>(this)) = NULL;
+    const FProperty* p_MyEquipment = GetClass()->FindPropertyByName("MyEquipment");
+    (*p_MyEquipment->ContainerPtrToValuePtr<UTresEquipmentComponent*>(this)) = NULL;
+    const FProperty* p_MyEffectAtt = GetClass()->FindPropertyByName("MyEffectAtt");
+    (*p_MyEffectAtt->ContainerPtrToValuePtr<UTresEffectAttachComponent*>(this)) = NULL;
+    this->MyTeam = ETresTeam::CityNpc;
+    this->m_bEnableRegistFootStepEffectGen = false;
+    const FProperty* p_ObstacleComponent = GetClass()->FindPropertyByName("ObstacleComponent");
+    (*p_ObstacleComponent->ContainerPtrToValuePtr<UMercunaObstacleComponent*>(this)) = NULL;
+    this->m_IdleAnimName = TEXT("idleN");
+    this->m_BodyColType = 0;
+    this->m_PoseGroupType = 0;
+    this->m_bBodyImmovable = true;
+    this->m_bDisableStagger = true;
+    this->m_bEnableBgCave = false;
+    this->m_bDisableReactor = false;
+    this->m_bDisableBodyCollision = false;
+    this->m_bDisableNavModifier = true;
+    this->m_bDisableLookAt = false;
+    this->m_bUniqueComNpc = false;
+    this->m_bForceAddComAction = false;
+    this->m_bDisableTurn = false;
+    this->m_ReactionType = -1;
+    this->m_MatData = NULL;
+    this->m_WalkSpeed = 0.00f;
+    this->m_RunSpeed = 0.00f;
+    this->m_fNearCameraRange = 0.00f;
+    this->m_LookAtBoneName = TEXT("atama");
+    this->m_FaceEyeBlinkMinTimer = 3.00f;
+    this->m_FaceEyeBlinkMaxTimer = 4.00f;
+    this->m_IdleBreakMinTimer = 4.00f;
+    this->m_IdleBreakMaxTimer = 9.00f;
+    this->m_fTurnWaitTimer = 3.00f;
+    this->m_fLookAtBlendTime = 0.50f;
+    this->m_fMinDegree = 10.00f;
+    this->m_fAddYaw = 0.00f;
+    this->m_bDispDebugInfo = false;
+    this->MyReactor = NULL;
+}
 
 void ATresNpcPawn_c_npc::SetSubMeshFacePlay(FName inSubMeshName, FName InLipAnimName, FName inEyeAnimName, float in_fBlendTime) {
 }
@@ -57,35 +104,4 @@ void ATresNpcPawn_c_npc::BP_ResetLookAt() {
 void ATresNpcPawn_c_npc::BP_ReqEndMotion() {
 }
 
-ATresNpcPawn_c_npc::ATresNpcPawn_c_npc(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-    this->m_IdleAnimName = TEXT("idleN");
-    this->m_BodyColType = 0;
-    this->m_PoseGroupType = 0;
-    this->m_bBodyImmovable = true;
-    this->m_bDisableStagger = true;
-    this->m_bEnableBgCave = false;
-    this->m_bDisableReactor = false;
-    this->m_bDisableBodyCollision = false;
-    this->m_bDisableNavModifier = true;
-    this->m_bDisableLookAt = false;
-    this->m_bUniqueComNpc = false;
-    this->m_bForceAddComAction = false;
-    this->m_bDisableTurn = false;
-    this->m_ReactionType = -1;
-    this->m_MatData = NULL;
-    this->m_WalkSpeed = 0.00f;
-    this->m_RunSpeed = 0.00f;
-    this->m_fNearCameraRange = 0.00f;
-    this->m_LookAtBoneName = TEXT("atama");
-    this->m_FaceEyeBlinkMinTimer = 3.00f;
-    this->m_FaceEyeBlinkMaxTimer = 4.00f;
-    this->m_IdleBreakMinTimer = 4.00f;
-    this->m_IdleBreakMaxTimer = 9.00f;
-    this->m_fTurnWaitTimer = 3.00f;
-    this->m_fLookAtBlendTime = 0.50f;
-    this->m_fMinDegree = 10.00f;
-    this->m_fAddYaw = 0.00f;
-    this->m_bDispDebugInfo = false;
-    this->MyReactor = NULL;
-}
 
